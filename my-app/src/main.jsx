4fdfbe9c52b4228eba8170e4db341bd4ebffaf07
omoladeajala <shadowcoderx.02@gmail.com>
2026-02-14 16:57:07 +0100

    simplify rendering of My Favourite Food component in main entry point

--- my-app/src/main.jsx
+++ my-app/src/main.jsx
@@ -1,16 +1,12 @@
 import { createRoot } from "react-dom/client";
 
-export default function Hello() {
-  return (
-    <>
-      <h1>My Favourite Food</h1>
-      <ul>
-        <li>Bacon</li>
-        <li>Jamon</li>
-        <li>Noodles</li>
-      </ul>
-    </>
-  );
-}
-
-createRoot(document.getElementById("root")).render(<Hello />);
+createRoot(document.getElementById("root")).render(
+  <div>
+    <h1>My Favourite Food</h1>
+    <ul>
+      <li>Bacon</li>
+      <li>Jamon</li>
+      <li>Noodles</li>
+    </ul>
+  </div>,
+);
