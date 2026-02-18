95e44aa8ae9809d0e59b51b6d7f03607f9270c40
omoladeajala <shadowcoderx.02@gmail.com>
2026-02-18 02:06:36 +0100

    refactor: simplify main rendering logic by removing unused imports and components

--- my-app/src/main.jsx
+++ my-app/src/main.jsx
@@ -1,19 +1,3 @@
-import { createRoot } from "react-dom/client";
-import "./index.css";
-const pic10 = "./pic10.jpeg";
-const pic20 = "./pic20.jpeg";
-const pic22 = "./pic22.jpeg";
+import { createRoot } from "react";
 
-createRoot(document.getElementById("root")).render(
-  <>
-    <div>
-      <h1 className="heading">My Favourite Food</h1>
-
-      <div className="images">
-        <img src={pic10 + "?white"} alt="random" />
-        <img src={pic20} alt="gym" />
-        <img src={pic22} alt="music" />
-      </div>
-    </div>
-  </>,
-);
+createRoot(document.getElementById("root")).render();
