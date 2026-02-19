7bae2f3d67b445a55d9a0b4034ee5d658c0343f5
omoladeajala <shadowcoderx.02@gmail.com>
2026-02-19 11:17:43 +0100

    refactor: update main component to utilize doublePi and triplePi functions for dynamic rendering

--- third-project/src/main.jsx
+++ third-project/src/main.jsx
@@ -1,12 +1,12 @@
 import { createRoot } from "react-dom/client";
-import pi from "./math.jsx";
+import pi, { doublePi, triplePi } from "./math.jsx";
 
 createRoot(document.getElementById("root")).render(
   <>
     <ul>
       <li>{pi}</li>
-      <li>2</li>
-      <li>3</li>
+      <li>{doublePi()}</li>
+      <li>{triplePi()}</li>
     </ul>
   </>,
 );
