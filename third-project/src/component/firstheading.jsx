4ce9672f4ee7ab23b59d5691c1d18434425aad6e
omoladeajala <shadowcoderx.02@gmail.com>
2026-02-19 10:59:43 +0100

    refactor: remove unused components and styles to streamline application structure

--- third-project/src/component/firstheading.jsx
+++
@@ -1,7 +0,0 @@
-export default function FirstHeading() {
-  return (
-    <>
-      <h3 className="text-">Our FirstHeading</h3>
-    </>
-  );
-}
--- third-project/src/main.jsx
+++ third-project/src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client";
+import pi from "./math.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+  <>
+    <ul>
+      <li>{pi}</li>
+      <li>2</li>
+      <li>3</li>
+    </ul>
+  </>,
+);
