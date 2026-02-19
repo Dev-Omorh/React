a17a511d827685a2911eb5dc739c5e8d53387fd4
omoladeajala <shadowcoderx.02@gmail.com>
2026-02-19 10:34:45 +0100

    refactor: restructure App component to utilize Heading and Greeting components

---
+++ my-app/src/Heading.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Heading() {
+  <h1 style={customStyle}>{greeting}</h1>;
+}
+
+export default Heading;
--- my-app/src/main.jsx
+++ my-app/src/main.jsx
@@ -1,19 +1,4 @@
 import { createRoot } from "react-dom/client";
+import App from "./App";
 
-const date = new Date();
-const currentHour = date.getHours();
-
-let greeting;
-const customStyle = {};
-
-if (currentHour < 12) {
-  greeting = "Good Morning";
-  customStyle.color = "red";
-} else if (currentHour < 18) {
-  greeting = "Good Afternoon";
-  customStyle.color = "green";
-} else {
-  greeting = "Good Night";
-}
-
-createRoot(document.getElementById("root")).render(<h1 style={customStyle}>{greeting}</h1>);
+createRoot(document.getElementById("root")).render(<App />);
