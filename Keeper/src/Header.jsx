7baa7986c96419b4c4b6f6692fd5486f3fd19393
omoladeajala <shadowcoderx.02@gmail.com>
2026-02-19 13:15:37 +0100

    Updated heading colors, borders, and footer layout

--- Keeper/src/Header.jsx
+++ Keeper/src/Header.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 function Header() {
-  return <h1 className="header">Keeper App</h1>;
+  return (
+    <>
+      <h1 className="header">Keeper App</h1>
+      <footer className="footer">Copyright@19</footer>
+    </>
+  );
 }
 
 export default Header;
