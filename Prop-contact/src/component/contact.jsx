7efcb280de864176e957f30404d5cb7114b101b6
omoladeajala <shadowcoderx.02@gmail.com>
2026-02-23 15:27:49 +0100

    Add image to public and use it inside contact component

---
+++ Prop-contact/src/component/contact.jsx
@@ -0,0 +1,38 @@
+const contacts = [
+  {
+    name: "Omolade",
+    imgURL: "/Prop-contact/public/IMG_8119.JPG",
+    phone: "+243 810 875 0414",
+    email: "omolade@gmail.com",
+  },
+  {
+    name: "Mercy",
+    imgURL: "Prop-contact/public/IMG-20250819-WA0160.jpg",
+    phone: "+243 906 684 6810",
+    email: "mercy@gmail.com",
+  },
+  {
+    name: "Prevail",
+    imgURL: "Prop-contact/public/IMG-20251125-WA0135.jpg",
+    phone: "+243 810 66 2224",
+    email: "prevail@gmail.com",
+  },
+  {
+    name: "Marvellous",
+    imgURL: "Prop-contact/public/IMG-20250825-WA0106.jpg",
+    phone: "+243 816 912 7693",
+    email: "marvellous@gmail.com",
+  },
+  {
+    name: "Precious",
+    imgURL: "Prop-contact/public/IMG-20250819-WA0107.jpg",
+    phone: "+243 814 372 0284",
+    email: "precious@gmail.com",
+  },
+  {
+    name: "Deborah",
+    imgURL: "Prop-contact/public/4401d609-974f-465b-b604-a26ccfa65d53.jpeg",
+    phone: "+243 803 831 5803",
+    email: "deborah@gmail.com",
+  },
+];
