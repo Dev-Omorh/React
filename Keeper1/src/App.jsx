5f30d79e4c24aca0510d5b8d44681c97adbcab2d
omoladeajala <shadowcoderx.02@gmail.com>
2026-02-23 04:20:34 +0100

    Adjust background styling for note and heading component

--- Keeper1/src/App.jsx
+++ Keeper1/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <>
       <Header />
-      <Note className="note" />
+      <Note />
       <Footer />
     </>
   );
--- Keeper1/src/component/Note.jsx
+++ Keeper1/src/component/Note.jsx
@@ -3,7 +3,7 @@ import "../App.css";
 
 function Note() {
   return (
-    <div className="note w-1/2 m-4 p-4 rounded bg-white">
+    <div className="note w-1/2 m-4 p-4 rounded bg-white shadow-lg">
       <h1>This is the note </h1>
       <p>This is the content of the note</p>
     </div>
